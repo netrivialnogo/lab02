30a2c9faf5c5b922ad70c69cf50aa594025fb2d0
netrivialnogo <kbelovwowcirkle@gmail.com>
2025-05-19 18:01:29 +0300

    add code comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,9 @@
+//Simple Hello World program that asks for user's name
 #include <iostream>
 #include <string>
 
 int main(){
-	std::string name;
+	std::string name; //Variable to store user's name
 	std::cout<<"Enter your name: ";
 	std::cin>>name;
 	std::cout<<"Hello world from "<<name<<std::endl;
