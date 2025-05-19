01deb78131a868590fcb773f1426475088b48c7e
netrivialnogo <kbelovwowcirkle@gmail.com>
2025-05-19 18:57:56 +0300

    Reformat code with clang-format Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //Простая программа Hello World, которая запрашивает имя пользователя
 #include <iostream>
 #include <string>
@@ -8,4 +9,18 @@ int main(){
 	std::cin>>name;
 	std::cout<<"Hello world from "<<name<<std::endl;
 	return 0;
+=======
+// Simple Hello World program that asks for user's name
+#include <iostream>
+#include <string>
+
+int
+main()
+{
+  std::string name; // Variable to store user's name
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
+>>>>>>> b5250f5 (Reformat code with clang-format Mozilla style)
 }
